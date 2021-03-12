9fdbfca9e4288bf957c931f645db64cfe4d80faa
Oan Stultjens <oan.stultjens@crosslang.com>
2021-03-12 23:04:06 +0100

    Initial commit, made models

---
+++ backend/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function manage_app () {
+    python manage.py makemigrations --merge --noinput &
+    python manage.py migrate --noinput &
+    python manage.py collectstatic --no-input --clear
+}
+
+function start_development() {
+    # use django runserver as development server here.
+    manage_app &
+    python manage.py runserver 0.0.0.0:8000
+}
+
+function start_production() {
+    # use gunicorn for production server here
+    manage_app &
+    gunicorn leadmanager.wsgi -w 4 -b 0.0.0.0:8000
+}
+
+if [ ${DJANGO_DEBUG} == "True" ]; then
+    # use development server
+    start_development
+else
+    # use production server
+    start_production
+fi
