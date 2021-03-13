b1fd93ddca0f1e3a6b15b0e1a2c9a88609052309
Oan Stultjens <oan.stultjens@crosslang.com>
2021-03-13 13:14:25 +0100

    Added coreapi for auto scheme generation and simplejwt for auth

--- backend/docmanager/urls.py
+++ backend/docmanager/urls.py
@@ -1,7 +1,22 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views import generic
+from rest_framework.schemas import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    url(r'^$', generic.RedirectView.as_view(
+         url='/api/', permanent=False)),
+    url(r'^api/$', get_schema_view()),
+    url(r'^api/auth/', include(
+        'rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/auth/token/obtain/$', TokenObtainPairView.as_view()),
+    url(r'^api/auth/token/refresh/$', TokenRefreshView.as_view()),
+
     path('admin/', admin.site.urls),
     path('documents/', include(('documents.urls', 'documents'), namespace='documents')),
 ]
