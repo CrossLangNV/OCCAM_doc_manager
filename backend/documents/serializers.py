03aeb06dfc41a6befab32cf9adf4d02cba65524b
Oan Stultjens <oan.stultjens@crosslang.com>
2021-04-28 15:43:39 +0200

    Fitbounds leaflet

--- backend/documents/serializers.py
+++ backend/documents/serializers.py
@@ -15,6 +15,16 @@ class PageSerializer(serializers.ModelSerializer):
     document = serializers.PrimaryKeyRelatedField(queryset=Document.objects.all())
     page_overlay = OverlaySerializer(many=True, read_only=True)
 
+    image_height = serializers.SerializerMethodField()
+    image_width = serializers.SerializerMethodField()
+    file = serializers.ImageField()
+
+    def get_image_height(self, page):
+        return page.file.height
+
+    def get_image_width(self, page):
+        return page.file.width
+
     class Meta:
         model = Page
         fields = "__all__"
--- frontend/src/containers/PageLeaflet.js
+++ frontend/src/containers/PageLeaflet.js
@@ -1,46 +1,59 @@
-import React from 'react';
-import {MapContainer, TileLayer, Marker, Popup, ImageOverlay} from 'react-leaflet'
+import React, {useRef} from 'react';
+import {MapContainer, TileLayer, Marker, Popup, ImageOverlay, useMap} from 'react-leaflet'
 import {useSelector} from "react-redux";
+import Leaflet from "leaflet";
+import {CRS} from "leaflet/dist/leaflet-src.esm";
 
 const PageLeaflet = (props) => {
-
-    const position = [51.505, -0.09]
-
-    // const page = props.page
-    // const file = props.file
-
-
+    const scale = .1; // TODO Change scale later
 
     const uiStates = useSelector(state => state.uiStates);
 
     const page = uiStates.selectedPage
     const file = uiStates.selectedPage.file
 
+    const mapRef = useRef(null);
 
-    console.log(page)
-    console.log(file)
+    const hw = (h, w) => {
+        if (Leaflet.Util.isArray(h)) {    // When doing xy([x, y]);
+            return hw(h[0], h[1]);
+        }
+        // let height go from top to bottom
+        return Leaflet.latLng(-h * scale, w * scale);  // When doing xy(x, y);
+    }
+
+
+    const center = hw([0, 0], null);
+    const width = page.image_width;
+    const height = page.image_height;
+
+    console.log(width)
+    console.log(height)
+
+
+    const imageBounds = [center, hw(height, width)];
+
+    function ResizeComponent() {
+        const map = useMap()
+        map.fitBounds(imageBounds)
+        return null
+    }
 
     return (
-        // <MapContainer center={[0, 0]} zoom={13}>
-        //
-        // </MapContainer>
-        <MapContainer center={[70, 50]} zoom={3} scrollWheelZoom={false}>
-            <Marker position={[100, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+        <MapContainer center={[0, 0]} zoom={3} scrollWheelZoom={false} crs={CRS.Simple}>
+
             <ImageOverlay
-                    url={file}
-                    bounds={[
-                        [100, 0],
-                        [-35, 100],
-                    ]}
-                    opacity={1}
-                    zIndex={10}
-                />
+                ref={mapRef}
+                url={file}
+                bounds={imageBounds}
+                opacity={1}
+                zIndex={10}
+            />
+
+            <ResizeComponent />
+
         </MapContainer>
-    );
+    )
 };
 
 export default PageLeaflet;
