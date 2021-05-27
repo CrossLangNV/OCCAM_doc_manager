437a115606794db607e153d850e2eb1913d9647e
Oan Stultjens <oan.stultjens@crosslang.com>
2021-05-27 10:07:33 +0200

    react env variables can only start with REACT_APP_ apparently

--- frontend/src/constants/axiosConf.js
+++ frontend/src/constants/axiosConf.js
@@ -1,8 +1,11 @@
 let baseUrl = "http://localhost:8000"
 
 if (process.env.NODE_ENV === 'production') {
-    baseUrl = process.env.DJANGO_BASE_URL
+    baseUrl = process.env.REACT_APP_API_URL
 }
+
+console.log(baseUrl)
+console.log(process.env.NODE_ENV)
 
 export {
     baseUrl
