5316db84638a4ced7ddcb862a62432f6b671aa4d
Oan Stultjens <oan.stultjens@crosslang.com>
2021-05-27 16:15:21 +0200

    Testing .env with docker compose

--- frontend/src/constants/axiosConf.js
+++ frontend/src/constants/axiosConf.js
@@ -1,8 +1,15 @@
 let baseUrl = "http://localhost:8000"
 
 if (process.env.NODE_ENV === 'production') {
-    baseUrl = "https://django.staging.occam.crosslang.com"
+    baseUrl = process.env.REACT_APP_API_URL
 }
+
+console.log(process.env.NODE_ENV)
+console.log(baseUrl)
+
+console.log(process.env.REACT_APP_API_URL)
+console.log(process.env.REACT_APP_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY)
+
 
 export {
     baseUrl
