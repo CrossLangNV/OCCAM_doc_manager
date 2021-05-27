ef5834cd59b5876902c1565f8bc5fe40c5709234
Oan Stultjens <oan.stultjens@crosslang.com>
2021-05-27 17:47:49 +0200

    Change process.env to window.env

--- frontend/src/constants/axiosConf.js
+++ frontend/src/constants/axiosConf.js
@@ -1,14 +1,14 @@
 let baseUrl = "http://localhost:8000"
 
 if (process.env.NODE_ENV === 'production') {
-    baseUrl = process.env.REACT_APP_API_URL
+    baseUrl = window._env_.REACT_APP_API_URL
 }
 
-console.log(process.env.NODE_ENV)
+console.log(window._env_.NODE_ENV)
 console.log(baseUrl)
 
-console.log(process.env.REACT_APP_API_URL)
-console.log(process.env.REACT_APP_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY)
+console.log(window._env_.REACT_APP_API_URL)
+console.log(window._env_.REACT_APP_SOCIAL_AUTH_GOOGLE_OAUTH2_KEY)
 
 
 export {
