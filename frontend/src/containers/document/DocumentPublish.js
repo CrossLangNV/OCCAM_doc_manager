1116f1a0b963a6e7d8e35b7eeacfcab740b3b086
Oan Stultjens <oan.stultjens@crosslang.com>
2021-06-23 17:02:33 +0200

    Added progressbar on the publish page

--- frontend/src/containers/document/DocumentPublish.js
+++ frontend/src/containers/document/DocumentPublish.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import ProgressBar from "../ProgressBar";
+import {Col, Row} from "react-bootstrap";
 
-const DocumentPublish = () => {
+const DocumentPublish = (props) => {
+    const documentId = props.match.params.documentId
+
     return (
-        <div>
-            Publish
-        </div>
+        <>
+            <Row>
+                <ProgressBar activeStep={5} documentId={documentId}/>
+            </Row>
+
+            <Row className="margin-top">
+                <Col>
+                    <h3>Publish</h3>
+                </Col>
+            </Row>
+        </>
     );
 };
 
