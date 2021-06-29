21e3d5806deb3a312903a647b25bfb566af5dbbb
Oan Stultjens <oan.stultjens@crosslang.com>
2021-06-29 11:17:10 +0200

    Implemented new setting: 'Show product tour'

--- frontend/src/containers/Settings.js
+++ frontend/src/containers/Settings.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, {useRef, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import _ from "lodash";
 import { FileUpload } from 'primereact/fileupload';
@@ -6,12 +6,19 @@ import { GetTmStats, UploadTMX } from '../actions/tmActions';
 import { Toast } from 'primereact/toast';
 import {Button} from "primereact/button";
 import {Col, Row, Table} from "react-bootstrap";
+import {ChangeTutorialState} from "../actions/authActions";
+import {InputSwitch} from "primereact/inputswitch";
 
 const Settings = () => {
+    // Redux
     const dispatch = useDispatch();
-    const toast = useRef(null);
     const tmStats = useSelector(state => state.tmStats);
+    const auth = useSelector(state => state.auth);
+
+    const toast = useRef(null);
     const uploadRef = useRef(null);
+    const [checkedTutorial, setCheckedTutorial] = useState(!auth.hasCompletedTutorial);
+
 
     React.useEffect(() => {
         fetchTmStats();
@@ -83,6 +90,17 @@ const Settings = () => {
                     uploadHandler={tmxUploader}
                     chooseLabel="Upload TMX"
                 />
+
+                <h2 className="margin-top">Product tour</h2>
+
+                <div className="p-field-checkbox">
+                    <InputSwitch inputId="enableTutorial" checked={checkedTutorial} onChange={e => {
+                        setCheckedTutorial(e.value)
+                        dispatch(ChangeTutorialState(auth.user, !e.value))
+                    }} />
+                    <label htmlFor="enableTutorial">Show product tour</label>
+                </div>
+
                 <Toast ref={toast} />
             </div>
         );
