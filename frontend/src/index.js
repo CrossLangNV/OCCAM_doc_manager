8a64ea2838c977a2861e4b199293a3c0022bf5f4
Oan Stultjens <oan.stultjens@crosslang.com>
2021-07-01 13:31:07 +0200

    Implemented Confluence Manual Page API

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,12 +7,16 @@ import {Provider} from "react-redux";
 import Store from "./Store";
 import {BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {I18nextProvider} from "react-i18next";
+import i18n from "./i18n";
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <Provider store={Store}>
-                <App/>
+                <I18nextProvider i18n={i18n}>
+                    <App/>
+                </I18nextProvider>
             </Provider>
         </BrowserRouter>
     </React.StrictMode>,
