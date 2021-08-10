13ec5fc5318835aba2a8144374394489e2ecb658
Oan Stultjens <oan.stultjens@crosslang.com>
2021-08-10 11:00:51 +0200

    Added rate_limit of 30/m

--- backend/scheduler/scraping_tasks.py
+++ backend/scheduler/scraping_tasks.py
@@ -1,12 +1,13 @@
 import os
 
-from celery import shared_task
 from scrapyd_api import ScrapydAPI
+
+from scheduler.celery import app
 
 scrapyd = ScrapydAPI(os.environ["SCRAPYD_URL"])
 
 
-@shared_task
+@app.task(rate_limit='30/m')
 def launch_scrapyd_throttled_request(website, settings, enterprise_number, user, *args, **kwargs):
     task = scrapyd.schedule('default', website, settings=settings,
                             company_number=enterprise_number,
