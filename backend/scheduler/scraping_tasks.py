7b2f8d988e430b50dd9c1a3eea9f15e1c0966419
Oan Stultjens <oan.stultjens@crosslang.com>
2021-08-10 11:32:46 +0200

    Moved the CSV file loop logic from django to celery. The ratelimiter now works for the whole process, django was getting timeouts because of the Response timeout waiting on the task to finish.

--- backend/scheduler/scraping_tasks.py
+++ backend/scheduler/scraping_tasks.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 from scrapyd_api import ScrapydAPI
@@ -6,9 +7,35 @@ from scheduler.celery import app
 
 scrapyd = ScrapydAPI(os.environ["SCRAPYD_URL"])
 
+"""
+    - With 1.5 million records:
+        - Rate limit of 240/m takes 10.5 days  
+        - Rate limit of 120/m takes 21 days  
+        - Rate limit of 60/m takes 42 days  
+"""
+
 
 @app.task(rate_limit='240/m')
-def launch_scrapyd_throttled_request(website, settings, enterprise_number, user, *args, **kwargs):
-    task = scrapyd.schedule('default', website, settings=settings,
-                            company_number=enterprise_number,
-                            user=user, website=website)
+def launch_scrapyd_throttled_request(website, settings, user, limit, *args, **kwargs):
+    with open(os.environ["ENTERPRISES_FILE_PATH"], "r") as csvfile:
+        csv_reader = csv.reader(csvfile)
+
+        count = 0
+
+        for row in csv_reader:
+
+            # Launch scraper for every record in the Staatsblad companies CSV file
+            enterprise_number = row[0]
+
+            if enterprise_number != "EnterpriseNumber":
+                enterprise_number = enterprise_number.replace(".", "")
+                print(f"Started scraping for enterprise number: {enterprise_number} (#{count})")
+
+                task = scrapyd.schedule('default', website, settings=settings,
+                                        company_number=enterprise_number,
+                                        user=user, website=website)
+
+                if limit and count == limit:
+                    break
+
+            count = count + 1
--- backend/scraper/views.py
+++ backend/scraper/views.py
@@ -1,4 +1,3 @@
-import csv
 import logging
 import os
 from uuid import uuid4
@@ -44,31 +43,7 @@ class LaunchScraperAPIView(APIView):
                 task = scrapyd.schedule('default', website, settings=settings,
                                         company_number=company_number, user=user, website=website)
             else:
-                with open(os.environ["ENTERPRISES_FILE_PATH"], "r") as csvfile:
-                    csv_reader = csv.reader(csvfile)
-
-                    count = 0
-
-                    for row in csv_reader:
-
-                        # Launch scraper for every record in the Staatsblad companies CSV file
-                        enterprise_number = row[0]
-
-                        if enterprise_number != "EnterpriseNumber":
-                            enterprise_number = enterprise_number.replace(".", "")
-                            print(f"Started scraping for enterprise number: {enterprise_number} (#{count})")
-
-                            launch_scrapyd_throttled_request.delay(website, settings, enterprise_number, user,
-                                                                   countdown=2)
-
-                            # task = scrapyd.schedule('default', website, settings=settings,
-                            #                         company_number=enterprise_number,
-                            #                         user=user, website=website)
-
-                            if limit and count == limit:
-                                break
-
-                        count = count + 1
+                launch_scrapyd_throttled_request.delay(website, settings, user, limit)
 
             response = {
                 "message": "Started scraper task",
