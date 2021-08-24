a080750195a2af8f94ec78a55dd5f38c6d82e28f
Oan Stultjens <oan.stultjens@crosslang.com>
2021-08-24 10:42:59 +0200

    WIP: implementing CTR scraper

--- backend/ctr/models.py
+++ backend/ctr/models.py
@@ -1,3 +1,78 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+
+class CtrCompany(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # General
+    identification_number = models.CharField(default="", max_length=100, unique=True)
+    date_of_creation_and_registration = models.CharField(default="", max_length=100)
+    file_number = models.CharField(default="", max_length=100)
+    trading_company = models.CharField(default="", max_length=100)
+    legal_form = models.CharField(default="", max_length=100)
+
+    class Meta:
+        verbose_name_plural = "CTR Companies"
+
+
+class Residence(models.Model):
+    address = models.CharField(default="", max_length=100)
+    company = models.ForeignKey(
+        CtrCompany,
+        related_name="ctr_company_residences",
+        on_delete=models.CASCADE,
+    )
+    date = models.CharField(default="", max_length=100)
+
+
+class ScopeOfBusiness(models.Model):
+    name = models.CharField(default="", max_length=100)
+    company = models.ForeignKey(
+        CtrCompany,
+        related_name="ctr_company_scope_of_businesses",
+        on_delete=models.CASCADE,
+    )
+    date = models.CharField(default="", max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Scope of businesses"
+
+
+class StatutoryAuthority(models.Model):
+    name = models.CharField(default="", max_length=100)
+    type = models.CharField(default="", max_length=100)
+    company = models.ForeignKey(
+        CtrCompany,
+        related_name="ctr_company_statutory_authorities",
+        on_delete=models.CASCADE,
+    )
+    date = models.CharField(default="", max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Statutory Authorities"
+
+
+class Companion(models.Model):
+    companion = models.CharField(default="", max_length=100)
+    share = models.CharField(default="", max_length=100)
+    lien = models.CharField(default="", max_length=100)
+    company = models.ForeignKey(
+        CtrCompany,
+        related_name="ctr_company_companions",
+        on_delete=models.CASCADE,
+    )
+    date = models.CharField(default="", max_length=100)
+
+
+class OtherFact(models.Model):
+    value = models.CharField(default="", max_length=100)
+    company = models.ForeignKey(
+        CtrCompany,
+        related_name="ctr_company_other_facts",
+        on_delete=models.CASCADE,
+    )
+    date = models.CharField(default="", max_length=100)
--- backend/scrapy_app/scrapy_app/spiders/ctr_spider.py
+++ backend/scrapy_app/scrapy_app/spiders/ctr_spider.py
@@ -4,7 +4,7 @@ import scrapy
 from bs4 import BeautifulSoup
 from scrapy import Selector
 
-from kbo.models import Company, BtwNacebelActivity, RszNacebelActivity, ExternalLink, LinkedEntity, Director
+from ctr.models import CtrCompany
 
 DATE_OF_CREATION_AND_ENTRY = "Datum vzniku a zápisu:"
 FILE_REFERENCE = "Spisová značka:"
@@ -28,59 +28,55 @@ LIEN = "Zástavní právo:"
 SHARE_CAPITAL = "Základní kapitál:"
 OTHER_FACTS = "Ostatní skutečnosti:"
 
+LEGAL_DOCUMENTS_URL = 'https://or.justice.cz/ias/ui/vypis-sl-firma?subjektId='
 COMPANY_INFO_BASE_URL = 'https://or.justice.cz/ias/ui/rejstrik-firma.vysledky?subjektId='
-COMPANY_INFO_PARAMS = '&typ=UPLNY'
-
+COMPANY_INFO_BASE_PARAMS = '&typ=UPLNY'
 
 EXAMPLE_COMPANY_SKODA = "423841"
 
 
-class KboSpider(scrapy.Spider):
-    name = 'KBO'
+class CtrSpider(scrapy.Spider):
+    name = 'CTR'
 
     def start_requests(self):
         url = COMPANY_INFO_BASE_URL
         company_number = getattr(self, 'company_number', None)
         if company_number is not None:
-            url = url + company_number
+            url = url + company_number + COMPANY_INFO_BASE_PARAMS
 
         print(f"Started '{self.name}' scraper for company number: ", company_number)
-        print("KBO Public Search URL: ", url)
-        yield scrapy.Request(url, self.parse_kbo_publications)
+        print("URL: ", url)
+        yield scrapy.Request(url, self.parse_ctr_publications)
 
-    def parse_kbo_publications(self, response):
+    def parse_ctr_publications(self, response):
         start = time.time()
 
-        company_number = str(self.company_number).strip()
-        print("Started scrapy 'parse_kbo_publications' request for company number: ", company_number)
+        identification_number = str(self.company_number).strip()
+        print("Started scrapy 'parse_ctr_publications' request for company number: ", identification_number)
 
         selector = Selector(text=response.text)
-        items = selector.xpath('//*[@id="table"]/table//tr')
+        # aunp-content
+        items = selector.xpath('//div[@class="aunp-content"]')
+        print("items: ", items)
 
         try:
-            company = Company.objects.get(company_number=company_number)
-        except Company.DoesNotExist:
-            company = Company.objects.create(company_number=company_number)
+            company = CtrCompany.objects.get(identification_number=identification_number)
+        except CtrCompany.DoesNotExist:
+            company = CtrCompany.objects.create(identification_number=identification_number)
 
+        # print("response.txt: ", response.text)
 
+        for item in items:
+            name = selector.xpath('//div[@class="vr-hlavicka"]/text()').extract()
+            print(f"name: {name}")
 
-        for item in items[1:]:
-            name = item.xpath('td[1]/text()').extract_first()
             value = item.xpath('td[2]/text()').extract()
             links = item.xpath('td[1]//a/@href').extract()
             links_text = item.xpath('td[1]//a/text()').extract()
 
             # Find external links and linked entities
             if links:
-                for i in range(len(links)):
-                    if any(map(links[i].__contains__, EXTERNAL_LINKS)):
-                        url = links[i]
-                        name = links_text[i]
-                        ExternalLink.objects.update_or_create(name=name, url=url, company=company)
-                    else:
-                        url = "https://kbopub.economie.fgov.be/kbopub/" + links[i]
-                        name = links_text[i]
-                        LinkedEntity.objects.update_or_create(name=name, url=url, company=company)
+                pass
 
             if not value:
                 # If value of text() is empty, it will probably be in another tag
@@ -94,64 +90,11 @@ class KboSpider(scrapy.Spider):
             if name:
                 print(f"{name}: {value}")
 
-                if name == STATE:
+                if name == LEGAL_STATUS:
                     company.state = value
 
                 elif name == LEGAL_STATUS:
                     company.legal_status = value
-
-                elif name == START_DATE:
-                    company.start_date = value
-
-                elif name == NAME:
-                    company.name = value
-
-                elif name == ADDRESS:
-                    value = item.xpath('td[2]').extract()
-                    value_stripped = remove_html_and_tabs(value[0])
-                    company.address = value_stripped
-
-                elif name == PHONE_NUMBER:
-                    company.phone_number = value
-
-                elif name == FAX_NUMBER:
-                    company.fax_number = value
-
-                elif name == EMAIL:
-                    company.email = value
-
-                elif name == WEBSITE:
-                    company.website = value
-
-                elif str(name) == ENTITY_TYPE:
-                    company.entity_type = value
-
-                elif name == LEGAL_FORM:
-                    company.legal_form = value
-
-                elif name == CAPITAL:
-                    company.capital = value
-
-                elif name == ANNUAL_MEETING:
-                    company.annual_meeting = value
-
-                elif name == FISCAL_YEAR_END_DATE:
-                    company.fiscal_year_end_date = value
-
-                elif name == START_DATE_EXCEPTIONAL_FINANCIAL_YEAR:
-                    company.start_date_exceptional_financial_year = value
-
-                elif name == END_DATE_EXCEPTIONAL_FINANCIAL_YEAR:
-                    company.end_date_exceptional_financial_year = value
-
-                elif name.startswith("BTW"):
-                    BtwNacebelActivity.objects.update_or_create(name=name, company=company)
-
-                elif name.startswith("RSZ"):
-                    RszNacebelActivity.objects.update_or_create(name=name, company=company)
-
-                elif name.startswith(tuple(DIRECTOR_PREFIXES)):
-                    Director.objects.update_or_create(name=value, role=name, company=company)
 
             company.save()
 
